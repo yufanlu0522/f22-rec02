3e50af12d24cbe823a6582dfb87f885c834c39f6
Yufan Lu <yufanlu@andrew.cmu.edu>
2023-01-25 14:15:35 -0500

    answered first question

--- java/src/main/java/edu/cmu/cs/cs214/rec02/Main.java
+++ java/src/main/java/edu/cmu/cs/cs214/rec02/Main.java
@@ -6,9 +6,9 @@ public class Main {
     public static void main(String[] args) {
         PolarPoint point1 = new PolarPoint(2, 0);
         PolarPoint point2 = new PolarPoint(4, Math.PI);
-        
+
         Line line = new Line(point1, point2);
-        
+
         line.draw();
     }
 }
